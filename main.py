cf8a6e27194884d1def362e03b226ec110f4cf37
d-Antao <d9antao@gmail.com>
2024-07-18 18:36:10 +0100

    add log

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 
 import qdarktheme
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+
 from buttons import Button, ButtonsGrid
 from display import Display, Info
 from main_window import MainWindow
 from styles import setupTheme
 from variables import WINDOW_ICON_PATH
-
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication
 
 if sys.platform.startswith('win'):
         import ctypes
@@ -20,7 +20,9 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     dark_stylesheet = qdarktheme.load_stylesheet('dark')  # ou dark
     window = MainWindow()
-   
+    
+    print("test")
+    
     #Icon difine   
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
